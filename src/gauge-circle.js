9108f0e78e0c9ac6250a3469748ef3fef09bb563
Lee-YuMin <lym91@hatiolab.com>
2016-04-25 20:23:07 +0900

    게이지 추가

--- src/gauge-circle.js
+++ src/gauge-circle.js
@@ -1,5 +1,5 @@
 function drawStepLine(context, ang, rx, needleSize) {
-  context.rotate(ang);
+  context.rotate(ang)
   context.translate(0, - rx)
 
   context.fillRect(0, -rx * 0.14, needleSize, rx * 0.175)
@@ -8,7 +8,7 @@ function drawStepLine(context, ang, rx, needleSize) {
 }
 
 function drawSubStepLine(context, ang, rx, needleSize) {
-  context.rotate(ang);
+  context.rotate(ang)
   context.translate(0, - rx)
 
   context.fillRect(0, -rx * 0.04, needleSize, rx * 0.075)
@@ -17,7 +17,7 @@ function drawSubStepLine(context, ang, rx, needleSize) {
 }
 
 function drawStepText(context, text, ang, rx) {
-  context.rotate(ang);
+  context.rotate(ang)
   context.translate(0, - rx * 0.83)
   context.rotate(-ang)
 
@@ -49,15 +49,17 @@ export default class GaugeCircle extends scene.Donut {
       fillStyle,
       textFillStyle = 'black',
       needleFillStyle = 'black',
+      innerCircleFillStyle = 'gray',
       needleSize = 2,
       stepFillStyle,
+      subTextSize,
       cx, cy, rx, ry, ratio
     } = this.model
 
     const rxRatio = rx / 100 * ratio  // 원 안에 지워지는 비율을 계산한 rx - ratio의 비율에 따라 크기가 변함
     const ryRatio = ry / 100 * ratio  // 원 안에 지워지는 비율을 계산한 ry - ratio의 비율에 따라 크기가 변함
-    const startAngle = (1.5 - angle / 180 * 0.5) * Math.PI  //  맨 위쪽을 중심으로 앵글의 범위에 따라 왼쪽으로 넓어짐
-    const endAngle   = (1.5 + angle / 180 * 0.5) * Math.PI  //  맨 위쪽을 중심으로 앵글의 범위에 따라 오른쪽으로 넓어짐
+    const startAngle = (1.5 - angle / 360)  //  맨 위쪽을 중심으로 앵글의 범위에 따라 왼쪽으로 넓어짐
+    const endAngle   = (1.5 + angle / 360)  //  맨 위쪽을 중심으로 앵글의 범위에 따라 오른쪽으로 넓어짐
     const circleSize = angle / 180     // 원의 총 길이. - PI * 2가 원이므로 (360도 = 2, 180도 = 1)
 
 
@@ -67,8 +69,8 @@ export default class GaugeCircle extends scene.Donut {
     ////  메인 게이지 원 그리  ////
     context.beginPath()
 
-    context.ellipse(0, 0, Math.abs(rx), Math.abs(ry), 0, startAngle, endAngle)
-    context.ellipse(0, 0, Math.abs(rxRatio), Math.abs(ryRatio), 0, endAngle, startAngle, true)  // 반대로 그리며 원을 지움.
+    context.ellipse(0, 0, Math.abs(rx), Math.abs(ry), 0, startAngle * Math.PI, endAngle * Math.PI)
+    context.ellipse(0, 0, Math.abs(rxRatio), Math.abs(ryRatio), 0, endAngle * Math.PI, startAngle * Math.PI, true)  // 반대로 그리며 원을 지움.
     
     this.drawFill(context)
     this.drawStroke(context)
@@ -81,8 +83,8 @@ export default class GaugeCircle extends scene.Donut {
       let beforeValue = startValue
       fillStep.forEach(v =>{
         context.beginPath()
-        let startStepAngle = startAngle + Math.PI * (circleSize * beforeValue / (endValue - startValue))
-        let endStepAngle = startAngle + Math.PI * (circleSize * v.value / (endValue - startValue))
+        let startStepAngle = Math.PI * (startAngle + circleSize * beforeValue / (endValue - startValue))
+        let endStepAngle = Math.PI * (startAngle + circleSize * v.value / (endValue - startValue))
 
         context.moveTo(0, 0)
         context.ellipse(0, 0, Math.abs(rx), Math.abs(ry), 0, startStepAngle, endStepAngle)
@@ -97,31 +99,37 @@ export default class GaugeCircle extends scene.Donut {
       })
     }
 
+    context.scale(1, ry / rx)
+    
+
+    ////  바늘 그리기  ////
+    value = Math.max(Math.min(value, endValue), startValue)   // 값이 startValue보다 작을 수 없고, endValue보다 클 수 없음.
+    let ang = Math.PI * (circleSize * (value + startValue) / (endValue - startValue) + startAngle - 0.5)
+
+    context.rotate(ang)
+
+    context.beginPath()
+
+    context.moveTo(rx * 0.02, 0)
+
+    context.lineTo(0, rx * 0.8)
+
+    context.lineTo(-rx * 0.02, 0)
+    context.lineTo(-rx * 0.01, -rx * 0.13)
+    context.lineTo(rx * 0.01, -rx * 0.13)
+
+    context.rotate(-ang)
+
+    context.fillStyle = needleFillStyle
+    context.fill()
+    context.closePath()
+
 
     ////  작은 원 그리기  ////
     context.beginPath()
-    context.ellipse(0, 0, Math.abs(rx) / 30, Math.abs(ry) / 30, 0, 0, 2 * Math.PI)
-    context.fillStyle = needleFillStyle
+    context.ellipse(0, 0, Math.abs(rx) / 20, Math.abs(rx) / 20, 0, 0, 2 * Math.PI)
+    context.fillStyle = innerCircleFillStyle
     context.fill()
-
-
-    context.scale(1, 1)
-
-
-    ////  바늘 그리기  ////
-    var needleLocation = (value - startValue) / (endValue - startValue) * Math.PI * 1.5
-    needleLocation = Math.max(Math.min(needleLocation, Math.PI * circleSize), 0)  // 0 보다 크고 PI * 1.5 보다 작게 만듦 
-
-    context.lineWidth = rx * 0.02
-    
-    context.moveTo(0, 0)
-    context.lineTo(rx * 0.8 * Math.cos(Math.PI * 0.75 + needleLocation) , ry * 0.8 * Math.sin(Math.PI * 0.75 + needleLocation))
-    
-    context.strokeStyle = needleFillStyle
-    context.stroke()
-    context.closePath()
-
-    context.scale(1, ry / rx)
 
 
     ////  스텝 선 그리기  ////
@@ -130,16 +138,15 @@ export default class GaugeCircle extends scene.Donut {
       let count = (endValue - startValue) / step
 
       // Draw StartValue 
-      drawStepLine(context, Math.PI * 0.7, rx * 0.8, needleSize)
-
+      drawStepLine(context, Math.PI * (startAngle + 0.5), rx * 0.8, needleSize) // 원을 그릴때 PI는 45도 부터 그리지만 angle은 0도부터 틀어서 + 0.5도를 곱해줘야함
       // Draw StepValue
       for(let num = 1; num < count; num++){
-        let ang = Math.PI * (1.5 / count * num) + 1.25 * Math.PI
+        let ang = Math.PI * (circleSize / count * num + startAngle + 0.5)
 
         drawStepLine(context, ang, rx * 0.8, needleSize)
       }
       // Draw EndValue
-      drawStepLine(context, Math.PI * 2.75, rx * 0.8, needleSize)
+      drawStepLine(context, Math.PI * (endAngle + 0.5), rx * 0.8, needleSize)
     }
 
 
@@ -147,35 +154,34 @@ export default class GaugeCircle extends scene.Donut {
     if(showSubStep){
       let count = endValue - startValue
 
-      // Draw StartValue     
-      drawSubStepLine(context, Math.PI * 1.25, rx * 0.8, needleSize)
+      // Draw StartValue
+      drawSubStepLine(context, Math.PI * (startAngle + 0.5), rx * 0.8, needleSize)
       
       // Draw StepValue
       for(let num = 1; num < count; num++){
         if(num % step == 0 || num % subStep != 0){  // 메인 스탭과 서브 스탭은 그리지 않음
           continue;
         }
-
-        let ang = Math.PI * (1.5 / count * num) + 1.25 * Math.PI
+        let ang = Math.PI * (circleSize / count * num + startAngle + 0.5)
 
         drawSubStepLine(context, ang, rx * 0.8, needleSize)
       }
       // Draw EndValue
-      drawSubStepLine(context, Math.PI * 2.75, rx * 0.8, needleSize)
+      drawSubStepLine(context, Math.PI * (endAngle + 0.5), rx * 0.8, needleSize)
     }
 
 
     ////  스텝 텍스트 그리기  ////
     context.fillStyle = textFillStyle
-    context.font = rx * 0.1 + "px arial"
+    context.font = rx * subTextSize / 50 + "px arial"
     context.textBaseline = "middle"
     context.textAlign = "center"
     if(showStartText){  // Draw StartText
-      drawStepText(context, startValue, Math.PI * 1.25, rx * 0.8)
+      drawStepText(context, startValue, Math.PI * (startAngle + 0.5), rx * 0.8)
     }
 
     if(showEndText){   // Draw EndText
-      drawStepText(context, endValue, Math.PI * 2.745, rx * 0.8)
+      drawStepText(context, endValue, Math.PI * (endAngle + 0.5), rx * 0.8)
     }
 
     if(showStepText){  // Draw StepText
@@ -183,12 +189,13 @@ export default class GaugeCircle extends scene.Donut {
 
       for(let num = 1; num < count; num++){
         let value = startValue + step * num
-        let ang = Math.PI * (1.5 / count * num) + 1.25 * Math.PI
+        let ang = Math.PI * (circleSize / count * num + startAngle + 0.5)
 
         drawStepText(context, value, ang, rx * 0.8)
       }
     }
 
+    context.scale(1, 1)
     context.translate(-cx, -cy)
   }
 
